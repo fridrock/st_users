a272222d7d20adadebf5f4f59aadf8a03e7ddb89
Александр <fridrix.aleksandr.2004@mail.ru>
2024-11-29 01:03:29 +0300

    changed cors policy

--- src/main/java/ru/evanemo/st_user/security/SecurityConfig.java
+++ src/main/java/ru/evanemo/st_user/security/SecurityConfig.java
@@ -3,7 +3,9 @@ package ru.evanemo.st_user.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +16,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -33,14 +41,27 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
     return httpSecurity
         .csrf(AbstractHttpConfigurer::disable)
+        .cors(Customizer.withDefaults())
         .authorizeHttpRequests((request)->request
             .requestMatchers("/users/**").permitAll()
-            .requestMatchers("/groups/**").hasAuthority("TEACHER")
-            .anyRequest().permitAll())
+            .requestMatchers("/groups/**").hasAuthority("TEACHER").anyRequest().permitAll()
+        )
+
         .sessionManagement(httpSecuritySessionManagementConfigurer -> {
           httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         })
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
+  }
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("http://localhost:3000"));
+    config.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "OPTIONS"));
+    config.addAllowedHeader("*");
+    config.setAllowCredentials(true);
+    source.registerCorsConfiguration("/**", config);
+    return source;
   }
 }
